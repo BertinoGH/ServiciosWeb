5974d8442766e228ea8ecf6f01a782f9e170ae08
BertinoGH <bertino_gh10@hotmail.com>
2023-03-08 13:55:38 -0600

    Phonebook

--- notas/src/components/App.js
+++ notas/src/components/App.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Note from './Note';
+import axios from 'axios';
 
-const App = ({ notes }) => {
-    return (
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          {notes.map(x => {
-            console.log(x.id, x.content);
-            return (
-              <Note key={x.id} note={x}/>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const handleNoteChange = (event) => {
+    //console.log(event.target.value)
+    setNewNote(event.target.value)
   }
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    //console.log('BUtton clicked',event)
+  }
+
+  const [showAll, setShowAll] = useState(true)
+  useEffect(() => {
+    console.log('Entro al effect')
+    axios
+      .get("http://localhost:3001/notes")
+      .then(response => {
+        console.log('Entro al then')
+        setNotes(response.data)
+      })
+  })
+  const noteToShow = showAll ? notes : notes.filter(x => x.important)
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>{showAll ? 'Important' : 'All'}</button>
+      <ul>
+        {noteToShow.map(x => {
+          console.log(x.id, x.content);
+          return (
+            <Note key={x.id} note={x} />
+          )
+        })}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit' >Save </button>
+
+      </form>
+    </div>
+  )
+
+}
 
 export default App
--- notas/src/index.js
+++ notas/src/index.js
@@ -2,31 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: '2023-02-14',
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JAVAsCRIPT",
-    date: '2023-02-15',
-    important: false
-  },
-  {
-    id: 3,
-    content: "get AND POST are the most importan methods",
-    date: '2023-02-15',
-    important: true
-  }
-
-]
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App notes={notes} />
+    <App />
   </React.StrictMode>
 );
