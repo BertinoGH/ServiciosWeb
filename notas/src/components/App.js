5049af7df87c0fa80da84d02d1a90eedbc761419
BertinoGH <bertino_gh10@hotmail.com>
2023-03-22 14:15:49 -0600

    1

--- notas/src/components/App.js
+++ notas/src/components/App.js
@@ -1,48 +1,70 @@
 import React, { useState, useEffect } from "react";
 import Note from './Note';
-import axios from 'axios';
+import noteService from '../services/notes'
+import Notification from "./Notifications";
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage,setErrorMessage]=useState(null)
   const handleNoteChange = (event) => {
     //console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
-
+      important: Math.random() < 0.5
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-    //console.log('BUtton clicked',event)
+    noteService
+    .create(noteObject)
+    .then(response=>{
+      setNotes(notes.concat(response))
+      setNewNote('')
+    })
   }
 
-  const [showAll, setShowAll] = useState(true)
   useEffect(() => {
-    console.log('Entro al effect')
-    axios
-      .get("http://localhost:3001/notes")
-      .then(response => {
-        console.log('Entro al then')
-        setNotes(response.data)
-      })
+    noteService
+    .getAll()
+    .then(response=>{setNotes(response)})
   })
+
   const noteToShow = showAll ? notes : notes.filter(x => x.important)
+
+  const updateImportance=(id)=>{
+    const note= notes.find(x=>x.id===id)
+    const note2= {...note,important:!note.important}
+    noteService
+    .update(id,note2)
+    .then(response=>{
+      setNotes(notes.map(x=>x.id!==id? x:response))
+    })
+    .catch(error=>{
+      setErrorMessage(`the note ${note2.content} was already existed`)
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },5000)
+      setNotes(notes.filter(x=>x.id !== id))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <button onClick={() => setShowAll(!showAll)}>{showAll ? 'Important' : 'All'}</button>
       <ul>
         {noteToShow.map(x => {
           console.log(x.id, x.content);
           return (
-            <Note key={x.id} note={x} />
+            <Note key={x.id} 
+            note={x}
+            updateImportance={()=>updateImportance(x.id)} />
           )
         })}
       </ul>
--- notas/src/components/Note.js
+++ notas/src/components/Note.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-const Note=({note}) => <li>{note.content}</li>
+const Note = ({ note, updateImportance }) => {
 
+    
+    const label = note.important
+        ? "make not important" : "make important"
+
+    return (
+        <li className="note">
+            {note.content}
+            <button onClick={updateImportance}>{label}</button>
+        </li>
+    )
+
+}
 export default Note
